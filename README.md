1772467c302541781c6594c4a4309e55f659afaa
sniperbuddha <60448430+sniperbuddha@users.noreply.github.com>
2021-10-31 18:19:19 +0100

    Error handling

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-The combination of a high demand for activities and an unreliable booking led to this. Run the program, and you should have booked a slot within one minute of it being possible. Might need to `pip install requests`.     
+The combination of a high demand for activities and an unreliable booking system led to this. Run the program, and you should have booked a slot within one minute of it being possible. Might need to `pip install requests`.     
--- autobooking.py
+++ autobooking.py
@@ -8,8 +8,9 @@ from datetime import timedelta, date
 import time
 
 credentials = {"username": "MY_USERNAME", "password": "MY_PASSWORD"}
-activity_name = 'Volleyboll Nybörjare'
+activity_name = 'MY_ACTIVITY'
 
+book_day = date.today() + timedelta(days=7)
 login_url = "https://ssif.brpsystems.com/brponline/api/ver3/auth/login"
 slots_url = "https://ssif.brpsystems.com/brponline/api/ver3/businessunits/1/groupactivities"
 headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
@@ -32,19 +33,21 @@ def try_to_book():
 
         # POST login
         r = s.post(login_url, json=credentials, headers=headers)
-        assert r.status_code == 200
+        if r.status_code != 200:
+            raise Exception("Login resulted in an error. Make sure the credentials are correct.")
         username = r.json()['username']
         token = f"Bearer {r.json()['access_token']}"
 
         # Get every slot on the day one week from today
-        start = date.today() + timedelta(days=7)
-        start_time = start.strftime('%Y-%m-%dT00:00:00.000Z')
-        stop_time = start.strftime('%Y-%m-%dT23:59:59.000Z')
+        start_time = book_day.strftime('%Y-%m-%dT00:00:00.000Z')
+        stop_time = book_day.strftime('%Y-%m-%dT23:59:59.000Z')
 
         # GET activities
         params = {'period.start': start_time, 'period.stop': stop_time, 'webCategory': 21}
         r = s.get(slots_url, params=params, headers=headers)
         assert r.status_code == 200
+        if r.status_code != 200:
+            raise Exception("Getting available activity slots resulted in an error.")
 
         # Make POST payload using activity id and token
         activity = activity_id(r.json())
@@ -59,9 +62,18 @@ def try_to_book():
 
 # Find the id using the activity name
 def activity_id(data):
-    for slot in data:
-        if slot['name'] == activity_name:
-            return slot['id']
+    res = -1
+    num_slots = 0
+    for i in range(len(data)):
+        if data[i]['name'] == activity_name:
+            num_slots += 1
+            res = data[i]['id']
+    if num_slots == 0:
+        raise Exception(f"No slots found for {activity_name} on date {book_day.strftime('%Y-%m-%d')}")
+    elif num_slots > 1:
+        raise Exception(f"Multiple ({num_slots}) slots found for {activity_name} on date {book_day.strftime('%Y-%m-%d')}")
+    else:
+        return res
 
 
 if __name__ == "__main__":
